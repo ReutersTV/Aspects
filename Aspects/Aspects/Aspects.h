0e3c958f90d20ed052184bb78316704e12b23a07
Martin Lloyd <martin.lloyd@thomsonreuters.com>
2015-06-01 17:32:16 +0100

    Add dynamic framework.

---
+++ Aspects/Aspects/Aspects.h
@@ -0,0 +1,19 @@
+//
+//  Aspects.h
+//  Aspects
+//
+//  Created by Martin Lloyd on 01/06/2015.
+//  Copyright (c) 2015 Thomson Reuters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Aspects.
+FOUNDATION_EXPORT double AspectsVersionNumber;
+
+//! Project version string for Aspects.
+FOUNDATION_EXPORT const unsigned char AspectsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aspects/PublicHeader.h>
+
+
